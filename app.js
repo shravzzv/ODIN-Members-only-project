241367d54a6ae0b2ff8703d07e487392eb33c083
sai shravan <saishravan384@gmail.com>
2024-05-11 10:02:51 +0530

    feat: Add authentication with sessions using Passport Local strategy.

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const passport = require('passport')
+const session = require('express-session')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 require('dotenv').config()
 
-const indexRouter = require('./routes/index')
-const dashboardRouter = require('./routes/dashboard')
 require('./config/db.config')
 require('./config/cloudinary.config')
+const User = require('./models/user')
+const indexRouter = require('./routes/index')
+const dashboardRouter = require('./routes/dashboard')
 
 const app = express()
 
@@ -21,6 +26,52 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
+// set up authentication middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+app.use(passport.session())
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username }, '_id password')
+      if (!user)
+        return done(null, false, {
+          message: `User with username "${username}" doesn't exist.`,
+        })
+
+      const match = await bcrypt.compare(password, user.password)
+      if (!match)
+        return done(null, false, {
+          message: `Incorrect password for username "${username}".`,
+        })
+
+      return done(null, user)
+    } catch (error) {
+      return done(error)
+    }
+  })
+)
+passport.serializeUser((user, done) => {
+  try {
+    return done(null, user.id)
+  } catch (error) {
+    return done(error)
+  }
+})
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id)
+    return done(null, user)
+  } catch (error) {
+    return done(error)
+  }
+})
 
 app.use('/', indexRouter)
 app.use('/dashboard', dashboardRouter)
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const Message = require('../models/message')
 const multerUtils = require('../utils/multer.util')
 const handleImage = require('../middlewares/handleImage.middleware')
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
 // The landing page home.
 exports.index = (req, res) => {
@@ -51,9 +52,9 @@ exports.userSignupPost = [
     .isEmail()
     .withMessage('Email is not a valid email address.')
     .escape()
-    .custom(async (value) => {
-      const existingUser = await User.findOne({ email: value })
-      if (existingUser) throw new Error(`E-mail "${value}" is already in use.`)
+    .custom(async (email) => {
+      const existingUser = await User.findOne({ email }, '_id')
+      if (existingUser) throw new Error(`E-mail "${email}" is already in use.`)
     }),
 
   body('username')
@@ -64,10 +65,10 @@ exports.userSignupPost = [
     .isLength({ max: 20 })
     .withMessage('username must be a maximum of 20 characters long.')
     .escape()
-    .custom(async (value) => {
-      const existingUser = await User.findOne({ username: value })
+    .custom(async (username) => {
+      const existingUser = await User.findOne({ username }, '_id')
       if (existingUser)
-        throw new Error(`Username "${value}" is already in use.`)
+        throw new Error(`Username "${username}" is already in use.`)
     }),
 
   body('password')
@@ -93,7 +94,7 @@ exports.userSignupPost = [
 
   body('bio').trim().optional().escape(),
 
-  asyncHandler(async (req, res) => {
+  asyncHandler(async (req, res, next) => {
     const errors = validationResult(req)
 
     // create User object with sanitized data
@@ -109,7 +110,7 @@ exports.userSignupPost = [
       user.password = hashedPassword
 
       await user.save()
-      res.redirect('signin')
+      next()
     } else {
       const allErrors = errors.array()
       if (req.imageError) allErrors.push(req.imageError)
@@ -124,21 +125,73 @@ exports.userSignupPost = [
       })
     }
   }),
+
+  passport.authenticate('local', {
+    // If a user is successfuly signed up, authentication never fails because it uses the same username and password from the saved database document. If user fails to signup correctly, this middleware never gets executed due to the conditions in the above middleware.
+
+    successRedirect: '/dashboard',
+  }),
 ]
 
 // Display signin form on GET.
 exports.userSigninGet = (req, res) => {
-  res.render('landing/signinForm', { title: 'Sign In' })
+  const errors = req?.session?.messages?.slice(-1)
+  // Here, errors needs to only handle invalid passwords because username errors are handled directly in the userSigninPost method. Passport pushes the error message to the req.session.messages, and we're only getting the latest error message.
+
+  res.render('landing/signinForm', {
+    title: 'Sign In',
+    errors,
+  })
 }
 
 // Handle signin on POST.
 exports.userSigninPost = [
-  asyncHandler(async (req, res) => {
-    res.send('User sign in post not implemented')
+  body('username')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('username must be atleast 3 characters long.')
+    .bail()
+    .isLength({ max: 20 })
+    .withMessage('username must be a maximum of 20 characters long.')
+    .escape()
+    .custom(async (username) => {
+      const user = await User.findOne({ username }, '_id')
+      if (!user) throw new Error(`Username "${username}" doesn't exist.`)
+    }),
+
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must be atleast 8 characters long.'),
+
+  asyncHandler((req, res, next) => {
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+      next()
+    } else {
+      const { username, password } = matchedData(req, { onlyValidData: false })
+
+      res.render('landing/signinForm', {
+        title: 'Sign In Error',
+        errors: errors.array(),
+        username,
+        password,
+      })
+    }
+  }),
+
+  passport.authenticate('local', {
+    failureRedirect: '/signin',
+    successRedirect: '/dashboard',
+    failureMessage: true,
   }),
 ]
 
 // Handle logout on GET.
 exports.userLogoutGET = asyncHandler(async (req, res, next) => {
-  res.send('user logout get not implemented')
+  req.logout((err) => {
+    if (err) return next(err)
+    res.redirect('/signin')
+  })
 })
